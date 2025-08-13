40329d28b38fa8a3c54eb27fb88db54c499b35f0
ArthurDias004 <arthurdodias@gmail.com>
2025-08-13 20:09:16 -0300

    Usúario entra com idade e nome

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,3 +1,7 @@
-﻿byte idade = 20;
-string nome = "Pedro";
+﻿byte idade ;
+string nome = "";
+Console.WriteLine(" Digite seu nome:");
+nome = Console.ReadLine();
+Console.Write("Digite sua idade");
+idade = Convert.ToByte(Console.ReadLine());
 Console.WriteLine("Ola " + nome + ". A idade informada foi {0}" , idade);
